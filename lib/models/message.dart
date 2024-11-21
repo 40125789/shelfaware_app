3272b1c8d41fc1b8c7813fe0e0832c08d0e17210
smyth184 <smyth668@hotmail.com>
2024-11-21 00:47:18 +0000

    I can send messages and it'll update in the chat, but individual chats for individual donation items still doesn't work

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -2,30 +2,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
   final String senderEmail;
-
+  final String receiverEmail;
   final String message;
   final String senderId;
   final String receiverId;
-  //final bool isRead;
+  final bool isRead;
   final Timestamp timestamp;
 
   Message({
     required this.senderEmail,
+    required this.receiverEmail,
     required this.message,
     required this.senderId,
     required this.receiverId,
-    //required this.isRead,
+    required this.isRead,
     required this.timestamp,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'senderEmail': senderEmail,
-      'message': message,
-      'senderId': senderId,
-      'receiverId': receiverId,
-      //'isRead': isRead,
-      'timestamp': timestamp,
-    };
+  factory Message.fromMap(Map<String, dynamic> data) {
+    return Message(
+      senderEmail: data['senderEmail'] ?? '',
+      receiverEmail: data['receiverEmail'] ?? '',
+      message: data['message'] ?? '',
+      senderId: data['senderId'] ?? '',
+      receiverId: data['receiverId'] ?? '',
+      isRead: data['isRead'] ?? false,
+      timestamp: data['timestamp'] ?? Timestamp.now(),
+    );
   }
 }
