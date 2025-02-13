bf42f5ae161145ded8948e19ce9a34120b3c23bd
smyth184 <smyth668@hotmail.com>
2025-02-13 19:42:12 +0000

    Refactor: Remove unused expiry notifier and notification handler services to streamline codebase

--- lib/services/expiry_notifier.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:shelfaware_app/services/notification_service.dart';
-import 'package:shelfaware_app/models/food_item.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
--- lib/ui/notification_handler.dart
+++
@@ -1,9 +0,0 @@
-// notification_handler.dart
-
-import 'package:flutter/material.dart';
-import 'package:shelfaware_app/models/food_item.dart'; // Corrected import path
-import 'package:shelfaware_app/services/expiry_notifier.dart';
-import 'package:shelfaware_app/services/data_fetcher.dart';
-
-
-
