038de0279c51d036dec00df4a971753ccae84ae7
smyth184 <smyth668@hotmail.com>
2025-05-05 12:24:55 +0100

    Enhance UserInput widget with dark mode support for text field and send button

--- lib/components/chat_user_input.dart
+++ lib/components/chat_user_input.dart
@@ -8,6 +8,9 @@ class UserInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brightness = Theme.of(context).brightness;
+    final isDarkMode = brightness == Brightness.dark;
+    
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
@@ -15,11 +18,25 @@ class UserInput extends StatelessWidget {
           Expanded(
             child: TextField(
               controller: messageController,
-              decoration: const InputDecoration(hintText: 'Type a message'),
+              decoration: InputDecoration(
+                hintText: 'Type a message',
+                hintStyle: TextStyle(
+                  color: isDarkMode ? Colors.grey[400] : null,
+                ),
+                fillColor: isDarkMode ? Colors.grey[800] : null,
+                filled: isDarkMode,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: isDarkMode ? Colors.grey[700]! : Colors.grey[300]!,
+                  ),
+                ),
+              ),
+              style: TextStyle(color: isDarkMode ? Colors.white : null),
             ),
           ),
           IconButton(
-            icon: const Icon(Icons.send),
+            icon: Icon(Icons.send, color: isDarkMode ? Colors.lightBlue[300] : null),
             onPressed: onSend,
           ),
         ],
